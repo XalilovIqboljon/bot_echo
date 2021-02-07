fb1fd4b15087047584142170e201862cb8d88e59
Xalilov <eljjey@gmail.com>
2021-02-07 15:45:44 +0500

    token os.envi

--- echo.py
+++ echo.py
@@ -1,13 +1,13 @@
-
+import os
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 
 def hello(update, context):
     update.message.reply_text('Assalom aleykum botga hush kelipsiz')
 
-
+token = os.environ['token_echo']
 updater = Updater(
-    '1623452611:AAHhPixDjQH2sre-onqGwwrBaamPZ5Uvips', use_context=True)
+    token, use_context=True)
 
 updater.dispatcher.add_handler(MessageHandler(Filters.text,hello))
 
